36d1fb66d87986963633124b271a1c4f129c2892
Gabriel Jackson <gabriel.d.jackson@gmail.com>
2013-09-20 08:43:21 -0400

    introduced basic configuration loading for site-specific settings

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 VoIP Tron!
 
-Version: 0.3
+Version: 0.4
 
 
 --- Description ---
@@ -11,7 +11,30 @@ VoIP Tron is a web-based data formatter used to create various VoIP-setup relate
 --- Installation ---
 
 1. Place files on a web server running PHP.
-2. Download the latest version of Bootstrap from http://twitter.github.com/bootstrap/ and unpack it to the root VoIP Tron directory.
-3. Go to index.php in a respectable web browser.
-4. ???
-5. Profit!
+2. Download Bootstrap 2.3.2 from http://getbootstrap.com/2.3.2/ and unpack it to the root VoIP Tron directory.
+3. Create voice.xml file in VOIPTRONROOT/config/voice.xml
+4. Go to index.php in a respectable web browser.
+5. ???
+6. Profit!
+
+
+--- Configuration ---
+
+Version 0.4 introduces basic configuration options.
+
+Call manager node IPs should now be entered into a voice.xml file at ./config/voice.xml
+evg.php will pull these IPs to create the proper dial peers.
+
+
+The following is a basic voice.xml template:
+
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<data>
+  <voice_services>
+    <cucm>
+      <ip>1.2.3.44</ip>
+      <ip>1.2.3.45</ip>
+      <ip>1.2.3.46</ip>
+    </cucm>
+  </voice_services>
+</data>
--- evg.php
+++ evg.php
@@ -25,36 +25,39 @@ array_walk($offices, 'add_building');
 header("Content-Type: text/txt; charset=utf-8");
 header("Content-Disposition: attachment; filename=" . $building . "-EVG.txt");
 
+$voiceXML = simplexml_load_file('config/voice.xml');
 
-$count = count($extensions);
-for ($i=0; $i<$count; $i++)
+if ($voiceXML)
 {
-  if (substr($extensions[$i], 0, 1) != "5")
+  $count = count($extensions);
+  for ($i=0; $i<$count; $i++)
   {
-    echo "!" . "\n";
-    echo "dial-peer voice " . $extensions[$i] . "1 voip" . "\n";
-    echo "preference 1" . "\n";
-    echo "destination-pattern " . $extensions[$i] . "\n";
-    echo "progress_ind disconnect enable 8" . "\n";
-    echo "voice-class codec 1" . "\n";
-    echo "session target ipv4:141.210.3.11" . "\n";
-    echo "dtmf-relay h245-alphanumeric" . "\n";
-    echo "no call fallback" . "\n";
-    echo "fax rate disable" . "\n";
-    echo "no vad" . "\n";
-    echo "!" . "\n";
-    echo "dial-peer voice " . $extensions[$i] . "2 voip" . "\n";
-    echo "preference 2" . "\n";
-    echo "destination-pattern " . $extensions[$i] . "\n";
-    echo "progress_ind disconnect enable 8" . "\n";
-    echo "voice-class codec 1" . "\n";
-    echo "session target ipv4:141.210.3.12" . "\n";
-    echo "dtmf-relay h245-alphanumeric" . "\n";
-    echo "no call fallback" . "\n";
-    echo "fax rate disable" . "\n";
-    echo "no vad" . "\n";   
-    echo "!" . "\n";
+    if (substr($extensions[$i], 0, 1) != "5")
+    {
+      $nodeCount = 0;
+      foreach($voiceXML->voice_services->cucm->ip as $key => $cucm_ip)
+      {
+        $nodeCount += 1;
+        echo "!" . "\n";
+        echo "dial-peer voice " . $extensions[$i] . $nodeCount . " voip" . "\n";
+        echo "preference " . $nodeCount . "\n";
+        echo "destination-pattern " . $extensions[$i] . "\n";
+        echo "progress_ind disconnect enable 8" . "\n";
+        echo "voice-class codec 1" . "\n";
+        echo "session target ipv4:" . $cucm_ip . "\n";
+        echo "dtmf-relay h245-alphanumeric" . "\n";
+        echo "no call fallback" . "\n";
+        echo "fax rate disable" . "\n";
+        echo "no vad" . "\n";
+      }
+      echo "!" . "\n";
+    }
   }
+}
+else
+{
+  echo "No voice.xml configuration found in .config/voice.xml\n\n";
+  echo "View README for details on how to build this file.";
 }
 
 
