bef372f449c378078fd4c412de79a51fb5256873
Gabriel Jackson <gabriel.d.jackson@gmail.com>
2015-05-07 09:36:56 -0700

    updated js links

--- interface-description-builder.php
+++ interface-description-builder.php
@@ -173,8 +173,10 @@
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="./bootstrap/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
 
 <?php
   if(isset($_GET['building']))
